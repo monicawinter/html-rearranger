cff1cdf28a405ddc893fe241e00d3473d2d06006
monicawinter <m.m.winter@hotmail.com>
2016-03-08 16:41:49 -0500

    started trying to add loops

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 var $newH1 = $('<h1>');
 $newH1.html('Below ground veggies');
 $('body').prepend($newH1);
+
+var $belowveggies = $('<ul>');
+$('body').append($belowveggies);
+
+var $aboveH1 = $('<h1>');
+$aboveH1.html('Above ground veggies');
+$('body').append($aboveH1);
+
+var $aboveveggies = $('<ul>');
+$('body').append($aboveveggies);
+
+$('ul li').each(function () {
+  $('.above').append('aboveveggies')
+}
