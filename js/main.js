c29bf66a76819c35ecf5596017384814f26b8f24
monicawinter <m.m.winter@hotmail.com>
2016-03-08 17:13:26 -0500

    rearranged js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 var $newH1 = $('<h1>');
+var $belowveggies = $('<ul>');
+var $aboveH1 = $('<h1>');
+var $aboveveggies = $('<ul>');
+
 $newH1.html('Below ground veggies');
 $('body').prepend($newH1);
 
-var $belowveggies = $('<ul>');
 $('body').append($belowveggies);
 
-var $aboveH1 = $('<h1>');
 $aboveH1.html('Above ground veggies');
 $('body').append($aboveH1);
 
-var $aboveveggies = $('<ul>');
 $('body').append($aboveveggies);
 
 $('ul li').each(function () {
-  if ($(this).hasClass('below')){
-    $belowveggies.append($(this))
+  if ($(this).hasClass('below')) {
+    $belowveggies.append($(this));
   } else {
-    $aboveveggies.append($(this))
+    $aboveveggies.append($(this));
   }
 });
